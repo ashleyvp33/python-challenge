ce0e2bc743978c5a541b18996d2ebd80a24cf075
ashleyvp33 <125237816+ashleyvp33@users.noreply.github.com>
2023-07-03 20:06:37 -0700

    beginning

---
+++ PyBank/main.py
@@ -0,0 +1,15 @@
+file = '../Resources/budget_data.csv'
+
+import os
+import csv 
+
+# PyBank
+# budget_data.csv
+print('Financial Analysis')
+print('----------------------------------------')
+# months = 
+# print( 'Total Months: ')
+# Total:
+# Average Change: $
+# Greatest Increase in Profits:
+# Greatest Decrease in Profits

5d1ee5acb8b813919b304983579532f22efdc381
ashleyvp33 <125237816+ashleyvp33@users.noreply.github.com>
2023-07-05 12:05:11 -0700

    csv

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,20 @@
+# PyBank
+# /Users/ashleypaillet/Desktop/git_HUB/03-python-challenge/python-challenge/PyBank
 file = '../Resources/budget_data.csv'
 
 import os
 import csv 
+csvpath = os.path.join('Resources', 'budget_data.csv')
 
-# PyBank
 # budget_data.csv
+with open(csvpath, encoding='UTF-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    # print(csvreader)
+    # csv_header = next(csvreader)
+    # print(f'CSV Header: {csv_header}')
+    # for row in csvreader:
+    #     print(row)
+
 print('Financial Analysis')
 print('----------------------------------------')
 # months = 
